199586ae03208582295211a66952f92c2c9fd07d
jurado91 <jurado91@bitbucket.org>
2016-05-21 12:32:17 -0500

    Changes

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var net = require('net');
 
-var server = net.createServer(function(socket) {
-	console.log('New Connection')
-	socket.write('Echo server\r\n');
-	socket.pipe(socket);
+var server = net.createServer();  
+server.on('connection', handleConnection);
+
+server.listen(9000, function() {  
+  console.log('server listening to %j', server.address());
 });
 
-server.listen(8090, '0.0.0.0');
+function handleConnection(conn) {  
+  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;
+  console.log('new client connection from %s', remoteAddress);
+
+  conn.on('data', onConnData);
+  conn.once('close', onConnClose);
+  conn.on('error', onConnError);
+
+  function onConnData(d) {
+    console.log('connection data from %s: %j', remoteAddress, d);
+    conn.write(d);
+  }
+
+  function onConnClose() {
+    console.log('connection from %s closed', remoteAddress);
+  }
+
+  function onConnError(err) {
+    console.log('Connection %s error: %s', remoteAddress, err.message);
+  }
+}
